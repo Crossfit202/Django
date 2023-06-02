57089d007f776939a870e101920fc5e1d1e2a061
Joncross202 <cro21035@byui.edu>
2023-06-01 23:20:25 -0400

    updated

--- listing/models.py
+++ listing/models.py
@@ -8,6 +8,13 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_author = models.CharField(max_length=100, default='unknown')
+    edition = models.CharField(max_length=100, default='unknown')
+    isbn = models.CharField(max_length=100, default='unknown')
+    condition = models.CharField(max_length=100, default='unknown')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    contact = models.CharField(max_length=100, default='unknown')
+    location = models.CharField(max_length=100, default='unknown')
 
 def __str__(self):
     return self.title
